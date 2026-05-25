482d070f1185ccd5641c7028d27bedeb176b08f6
shshoyqulov <shshoyqulov@gmail.com>
2026-05-25 08:27:14 +0500

    Add files via upload

--- students.js
+++ students.js
@@ -1,121 +1,108 @@
 const STUDENTS = [
-   { ism: "Tester", guruh: "tester01" },
 
-    // 022-45
-   { 
-      ism: "Nafisa", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
-   { 
-      ism: "Abdulahadxon", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
-   { 
-      ism: "Begali", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
-    { ism: "Asilbek", guruh: "022-45" },
-    { ism: "Aslbek Avl", guruh: "022-45" },
-    { ism: "Feruz", guruh: "022-45" },
+{ ism: "Tester", guruh: "tester01" },
+      // 023-41
     { 
-      ism: "Zuloyha", 
-      guruh: "022-45",
+      ism: "Kamol", 
+      guruh: "023-41",
       imtiyoz: true
     },
-    { ism: "Nurzod", guruh: "022-45" },
-    { ism: "Nurmuhammad", guruh: "022-45" },
-    { ism: "Shukurullo", guruh: "022-45" },
-   { 
-      ism: "Shukurullo", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
-    { ism: "Sevinch", guruh: "022-45" },
-    { ism: "Maqsud", guruh: "022-45" },
- { 
-      ism: "Yulduz", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
+    { ism: "Bekzod", guruh: "023-41" },
+    { ism: "Lola", guruh: "023-41" },
+    { ism: "Islomjon", guruh: "023-41" },
+    { ism: "Feroza", guruh: "023-41" },
+    { ism: "Jamshid", guruh: "023-41" },
+    { ism: "Ilxomjon", guruh: "023-41" },
   { 
-      ism: "Zumrad", 
-      guruh: "022-45",
+      ism: "Jumagul", 
+      guruh: "023-41",
       imtiyoz: true
     },
-    { ism: "Dilnura", guruh: "022-45" },
-  { 
-      ism: "Nasiba", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
-    { ism: "Ogabek", guruh: "022-45" },
-    { ism: "Oldost", guruh: "022-45" },
-    { ism: "Ruslan", guruh: "022-45" },
-    { ism: "Umid", guruh: "022-45" },
-   { 
-      ism: "Asilbek X", 
-      guruh: "022-45",
-      imtiyoz: true
-    },
-
-    // 022-46
-  { 
-      ism: "Ogabek", 
-      guruh: "022-46",
-      imtiyoz: true
-    },
-    { ism: "Mohira", guruh: "022-46" },
-    { ism: "Gulasal", guruh: "022-46" },
-    { ism: "Anvar", guruh: "022-46" },
-    { ism: "Muxlisa Bek.", guruh: "022-46" },
+    { ism: "Xurshid", guruh: "023-41" },
+    { ism: "Temur", guruh: "023-41" },
+    { ism: "Iroda", guruh: "023-41" },
 { 
-      ism: "Muxlisa Ber.", 
-      guruh: "022-46",
+      ism: "Ismoil", 
+      guruh: "023-41",
       imtiyoz: true
     },
-    { ism: "Dilshod", guruh: "022-46" },
-   { ism: "Davlatjon", guruh: "022-46" },
-   { ism: "Davlatmurod", guruh: "022-46" },
+    { ism: "Xusan", guruh: "023-41" },
+    { ism: "Hilola", guruh: "023-41" },
+{ 
+      ism: "Ezoza", 
+      guruh: "023-41",
+      imtiyoz: true
+    },
+    { ism: "Shaxobiddin", guruh: "023-41" },
+    { ism: "Setora", guruh: "023-41" },
+    { ism: "Ismadiyor", guruh: "023-41" },
+{ 
+      ism: "Ismadiyor", 
+      guruh: "023-41",
+      imtiyoz: true
+    },
+    { ism: "Surayyo", guruh: "023-41" },
+    { ism: "Fazliddin", guruh: "023-41" },
+
+    // 023-42
    { 
-      ism: "Feruza", 
-      guruh: "022-46",
+      ism: "Zilola", 
+      guruh: "023-42",
       imtiyoz: true
     },
-   { 
-      ism: "Gavhar", 
-      guruh: "022-46",
-      imtiyoz: true
-    },
-    { ism: "Javohir", guruh: "022-46" },
-    { ism: "Voxid", guruh: "022-46" },
-    { ism: "Fayyoza", guruh: "022-46" },
- { 
-      ism: "Sherali", 
-      guruh: "022-46",
-      imtiyoz: true
-    },
-  { 
-      ism: "Shabona", 
-      guruh: "022-46",
-      imtiyoz: true
-    },
+    { ism: "Barfiza", guruh: "023-42" },
     { 
-      ism: "Maftuna", 
-      guruh: "022-46",
+      ism: "Fotima", 
+      guruh: "023-42",
       imtiyoz: true
     },
-    { ism: "Ezoza", guruh: "022-46" },
-    { ism: "Rushana", guruh: "022-46" },
  { 
-      ism: "Saidnazarbek", 
-      guruh: "022-46",
+      ism: "Mashhura", 
+      guruh: "023-42",
       imtiyoz: true
     },
-    { ism: "Soxibjon", guruh: "022-46" },
-    { ism: "Oyziyod", guruh: "022-46" },
-    { ism: "Sevara", guruh: "022-46" },
-    { ism: "Abdurahmon", guruh: "022-46" }
+    { ism: "Hikmatulla", guruh: "023-42" },
+    { ism: "Gulchehra", guruh: "023-42" },
+    { ism: "Sevinch", guruh: "023-42" },
+    { ism: "Gulasal", guruh: "023-42" },
+    { ism: "Yulduz", guruh: "023-42" },
+     { 
+      ism: "Feruza", 
+      guruh: "023-42",
+      imtiyoz: true
+    },
+    { ism: "Nazokat", guruh: "023-42" },
+    { 
+      ism: "Durdona", 
+      guruh: "023-42",
+      imtiyoz: true
+    },
+{ 
+      ism: "Dilafroz", 
+      guruh: "023-42",
+      imtiyoz: true
+    },
+{ 
+      ism: "Asila", 
+      guruh: "023-42",
+      imtiyoz: true
+    },
+    { ism: "Iroda", guruh: "023-42" },
+    { ism: "Gulsanam", guruh: "023-42" },
+ { 
+      ism: "Sumbula", 
+      guruh: "023-42",
+      imtiyoz: true
+    },
+   { 
+      ism: "Shoxjaxon", 
+      guruh: "023-42",
+      imtiyoz: true
+    },
+    { ism: "Jasmina", guruh: "023-42" },
+  { 
+      ism: "Sabina", 
+      guruh: "023-42",
+      imtiyoz: true
+    }
 ];
