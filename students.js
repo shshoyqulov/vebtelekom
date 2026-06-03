bb4612d563ec477274dadc879efd0e80203e3cb3
shshoyqulov <shshoyqulov@gmail.com>
2026-06-03 09:25:50 +0500

    Add files via upload

--- students.js
+++ students.js
@@ -1,7 +1,70 @@
 const STUDENTS = [
 
-{ ism: "Tester", guruh: "tester01" },
+{ ism: "Tester", guruh: "tester01" , parol: "111"},
+      // 023-41
    
-   
-    
+    { ism: "Bekzod", guruh: "023-41", parol: "02341" },
+    { ism: "Islomjon", guruh: "023-41", parol: "02341" },
+    { ism: "Feroza", guruh: "023-41", parol: "02341" },
+    { ism: "Jamshid", guruh: "023-41", parol: "02341" },
+    { ism: "Ilxomjon", guruh: "023-41", parol: "02341" },
+     { ism: "Xurshid", guruh: "023-41", parol: "02341" },
+    { ism: "Temur", guruh: "023-41" , parol: "02341"},
+    { ism: "Iroda", guruh: "023-41" , parol: "02341"},
+    { ism: "Xusan", guruh: "023-41" , parol: "02341"},
+    { ism: "Hilola", guruh: "023-41", parol: "02341" },
+{ 
+      ism: "Ezoza", 
+      guruh: "023-41",
+     parol: "02341",
+      imtiyoz: true
+    },
+    { ism: "Setora", guruh: "023-41", parol: "02341" },
+    { ism: "Surayyo", guruh: "023-41, parol: "02341" },
+    { ism: "Fazliddin", guruh: "023-41, parol: "02341" },
+
+    // 023-42
+   { 
+      ism: "Zilola", 
+      guruh: "023-42",
+     parol: "02342",
+      imtiyoz: true
+    },
+    { ism: "Barfiza", guruh: "023-42" },
+    { 
+      ism: "Fotima", 
+       guruh: "023-42",
+     parol: "02342",
+      imtiyoz: true
+    },
+     { ism: "Hikmatulla", guruh: "023-42", parol: "02342" },
+     { ism: "Yulduz", guruh: "023-42", parol: "02342" },
+     { 
+      ism: "Feruza", 
+        guruh: "023-42",
+     parol: "02342",
+      imtiyoz: true
+    },
+    { ism: "Nazokat", guruh: "023-42", parol: "02342" },
+    { ism: "Durdona", guruh: "023-42", parol: "02342" },
+{ 
+      ism: "Dilafroz", 
+       guruh: "023-42",
+     parol: "02342",
+      imtiyoz: true
+    },
+{ 
+      ism: "Asila", 
+        guruh: "023-42",
+     parol: "02342",
+      imtiyoz: true
+    },
+    { ism: "IrodaU", guruh: "023-42", parol: "02342" },
+   { 
+      ism: "Shoxjaxon", 
+        guruh: "023-42",
+     parol: "02342",
+      imtiyoz: true
+    },
+    { ism: "Jasmina", guruh: "023-42", parol: "02342" }
  ];
