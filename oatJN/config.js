7dfc9bed7d722b3452ded512e973d737efc9f2ae
shshoyqulov <shshoyqulov@gmail.com>
2026-06-04 12:03:22 +0500

    Add files via upload

---
+++ oatJN/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    testNomi: "Optik aloqa tizimlari fanidan JN test topshiriqlari",
+    testSavollarSoni: 20,
+    javoblarSoni: 4,
+    testVaqti: 1300,
+
+    savollarRandom: true,
+    javoblarRandom: true,
+
+    aloFoiz: 90,
+    yaxshiFoiz: 70,
+    qoniqarliFoiz: 60,
+
+    txtSaqlash: true,
+    uySahifa: "../index.html"
+};
