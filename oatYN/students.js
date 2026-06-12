d9164b82d6bf45e75598465bf59d4ae1890468a0
shshoyqulov <shshoyqulov@gmail.com>
2026-06-12 07:22:40 +0500

    Add files via upload

---
+++ oatYN/students.js
@@ -0,0 +1,24 @@
+﻿const STUDENTS = [
+{ ism: "Admin", guruh: "administrator", parol: "admin2026",imtiyoz: true, imtiyozFoiz: 100 },
+{ ism: "Mehmon", guruh: "Tester", parol: "0101", imtiyoz: true, imtiyozFoiz: 95 },
+{ ism: "Mehmon2", guruh: "Tester", parol: "0101" },
+    // 022-16
+
+{ ism: "Aabdullayeva Shahzoda", guruh: "022-16", parol: "6122" },
+{ ism: "Ikromova Nozima", guruh: "022-16", parol: "6122" },
+{ ism: "Masharipova Bikajon", guruh: "022-16", parol: "6122" },
+{ ism: "Nortojiyeva Nozimabonu", guruh: "022-16", parol: "6122" },
+{ ism: "Sadullayev Sanjar", guruh: "022-16", parol: "6122" },
+{ ism: "Saidov Sherzod", guruh: "022-16", parol: "6122" },
+{ ism: "Tojiyeva Madina", guruh: "022-16", parol: "6122" },
+{ ism: "Turopova Madina", guruh: "022-16", parol: "6122" },
+{ ism: "Abdullayev Zarifboy", guruh: "022-16", parol: "6122"},
+{ ism: "Eshpulatov Diyorbek", guruh: "022-16", parol: "6122" },
+{ ism: "Jo‘rayev Axmed", guruh: "022-16", parol: "6122" },
+{ ism: "Lapasov Ravshan", guruh: "022-16", parol: "6122" },
+{ ism: "Nurmamatov Muhammad", guruh: "022-16", parol: "6122" },
+{ ism: "Olimjonov Azizbek", guruh: "022-16", parol: "6122" },
+{ ism: "Sapoatov Ramazon", guruh: "022-16", parol: "6122" },
+{ ism: "Tursunova Farangiz", guruh: "022-16", parol: "6122" },
+{ ism: "Xolpo‘latov Nurbek", guruh: "022-16", parol: "6122" }
+];
